0a1db78b7671d33fadc0584190f3c3cf83aa4b64
Matthew Ream <matthewtimothyream@gmail.com>
2019-09-09 11:24:03 -0500

    Done.

---
+++ lib/user.rb
@@ -0,0 +1,17 @@
+class User
+    attr_accessor :first_name, :last_name, :knowledge
+    
+    def initialize
+        @first_name = first_name
+        @last_name = last_name
+        @knowledge = []
+    end
+
+    def learn(string)
+        @knowledge << string
+    end
+    
+    def knowledge
+        @knowledge
+    end
+end
